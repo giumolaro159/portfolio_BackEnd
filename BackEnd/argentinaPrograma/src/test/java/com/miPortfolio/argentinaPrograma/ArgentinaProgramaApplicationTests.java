abce00cf6cd9659bd47c255044c55e1daa097820
Giuliana Molaro <123484973+giumolaro159@users.noreply.github.com>
2023-04-21 20:30:10 -0300

    Add files via upload

---
+++ BackEnd/argentinaPrograma/src/test/java/com/miPortfolio/argentinaPrograma/ArgentinaProgramaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miPortfolio.argentinaPrograma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArgentinaProgramaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
